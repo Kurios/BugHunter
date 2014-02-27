99090cb2e46086b0bbe26154d1f819d601cbe3f2
jacmorgan <jacmorgan@ebay.com>
2014-02-26 19:04:29 -0800

    Init

---
+++ Assasin.js
@@ -0,0 +1,80 @@
+
+/**
+ * Assasin: Generates a single Assasin. Required Vars: Name: Players Name.
+ * Location: Where the player resides. email: target players email.
+ * 
+ * Generated Values (here for loading on event of system crash) Nickname target:
+ * The current target of this assassin mark: Who's trying to kill off this
+ * assassin status: "UnInit" //Not in use now "Alive" "Dead"
+ */
+public function Assasin(name,location,email,target,mark,nickname,id,status) {
+	this.name = name;
+	this.location = location;
+	this.email = email;
+	
+	if(typeof nickname === 'undefined'){
+		nickname = generateNickName(nickname);
+	}
+	this.nickname = nickname;
+	if(!(typeof target === 'undefined'))
+		this.target = target;
+	if(!(typeof mark === 'undefined'))
+		this.mark = mark;
+	if(typeof status === 'undefined')
+		status = "Alive";
+	
+	if(typeof id === 'undefined')
+		id = generateID();
+	this.id = id;
+	this.status = status;
+	
+}
+
+
+/**
+ * target : Assasin to kill off
+ */
+Assasin.prototype.kill = function (target){
+	if(this.target == target.id ) // emails are unique
+		this.target = target.target;
+	else if(this.mark == target.id)
+	{
+		// Handle this edge case. Though really, it should be reassigning a
+		// target forward from the tail
+		//
+		// 1 > 2 > 3 > 4
+		//
+		// pop 2 (whos the mark) as 3 killed 2.
+		//
+		// 1> 3 > 4
+	}
+};
+
+/**
+ * TODO: A call to generate a random nick name
+ * 
+ */
+public function generateNickName(name)
+{
+	return "Zero";
+}
+
+/**
+ * TODO: Generate a unique nick id
+ */
+public function generateID()
+{
+	return 0;
+}
+
+
+public function generateHits(assasins,distanceFunction)th
+{
+	var distances
+}
+
+public function hameltonGraph(graph)
+{
+	
+}
+
